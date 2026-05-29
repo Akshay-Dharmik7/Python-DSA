bd7f5e094aec3365f88056679b775fb2dbdcbeff
Akshay-Dharmik7 <akshaydharmik72@gmail.com>
2026-05-29 13:52:44 +0530

    longest common prefix in strings

---
+++ 02_Longest_Common_Prefix/longest_common_prefix.py
@@ -0,0 +1,29 @@
+
+
+def longestCommonPrefix(strs):
+    if len(strs) == 0:
+        return ""
+    
+    result = ""
+    base = strs[0]
+
+    for i in range(len(base)):
+        for word in strs[1:]:
+            if len(word) == 0 or word[i] != base[i]:
+                return result
+        
+        result += base[i]
+    
+    return result
+
+
+n = int(input("How many string do you want to enter? "))
+strs = []
+
+for i in range(n):
+    item = input(f"Enter string {i+1}: ")
+    strs.append(item)
+
+print(strs)
+
+print(longestCommonPrefix(strs))
